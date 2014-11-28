52daf1b3009abb158f6844b367a32a464399e33c
nmalo5 <nmalo5@users.noreply.github.com>
2014-11-28 19:16:39 +0100

    pull request!
    He echado un ojo y no hay muchas pegas que poner..  te faltaría coger la IP de la linea de comandos para poder comunicarnos entre nosotros(si lo cambio yo también, que lo tengo igual que tú), como comentaba Raquel. Te he añadido otra línea de os.system porque a mi me daba errores al ejecutar sin esa linea. Si no te pasa, ni caso. Por último una variable para el sys.argv[3], pero eso ya es gusto personal :)

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
+        IP_CLIENT = str(self.client_address[0])
         while 1:
             line = self.rfile.read()
             if not line:
@@ -30,8 +31,9 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 respuesta += "SIP/2.0 200 OK\r\n\r\n"
                 self.wfile.write(respuesta)
             elif Method == 'ACK':
-                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + sys.argv[3]
+                aEjecutar = './mp32rtp -i ' + IP_CLIENT + ' -p 23032 < ' + fichero
                 print "Vamos a ejecutar", aEjecutar
+                os.system('chmod 755 mp32rtp')
                 os.system(aEjecutar)
                 print(" Ha terminado la ejecución de fich de audio")
             elif Method == 'BYE':
@@ -43,7 +45,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
     try:
         LISTEN_PORT = int(sys.argv[2])
-        sys.argv[3]
+        fichero = sys.argv[3]
     except IndexError:
         sys.exit("Usage: python server.py IP port audio_file")
     # Creamos servidor de eco y escuchamos
